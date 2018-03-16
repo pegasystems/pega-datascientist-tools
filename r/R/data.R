60169665d01a7f1206d35a7658898b1eb54442f6
Otto Perdeck <perdo@pega.com>
2018-03-16 17:15:58 +0100

    Including the Datamart samples

--- r/R/data.R
+++ r/R/data.R
@@ -1,6 +1,6 @@
-#' Sample model snapshot from the ADM Data Mart.
+#' Sample model snapshots from the ADM Data Mart.
 #'
-#' A \code{data.table} containing two snapshots of 15 models each.
+#' A \code{data.table} containing two snapshots of 3 models.
 #'
 #' @format A \code{data.table} with 30 rows and 22 variables:
 #' \describe{
@@ -10,3 +10,17 @@
 #' }
 #' @source A sample Pega application
 "admdatamart_models"
+
+#' Sample predictor binning snapshots from the ADM Data Mart.
+#'
+#' A \code{data.table} containing a snapshot of the predictor binning of the ADM models in \code{admdatamart_models}.
+#'
+#' @format A \code{data.table} with 1755 rows and 35 variables:
+#' \describe{
+#'   \item{pyModelID}{Model ID}
+#'   \item{pySnapshotTime}{Snapshot time}
+#'   \item{pyPredictorName}{Name of the predictor}
+#'   ...
+#' }
+#' @source A sample Pega application
+"admdatamart_binning"
--- r/tests/testthat/test_cdh_utils.R
+++ r/tests/testthat/test_cdh_utils.R
@@ -8,6 +8,10 @@ test_that("dataset export can be read", {
   expect_equal(nrow(data), 30)
 })
 
+# to add data:
 # admdatamart_models <- readDSExport("Data-Decision-ADM-ModelSnapshot_AllModelSnapshots", "~/Downloads")
+# names(admdatamart_models) <- tolower(names(admdatamart_models))
+# for(f in c("pyperformance")) admdatamart_models[[f]] <- as.numeric(admdatamart_models[[f]])
 # devtools::use_data(admdatamart_models)
 # save(admdatamart_models, file="data/admdatamart_models.rda", compress='xz')
+# + describe in R/data.R
