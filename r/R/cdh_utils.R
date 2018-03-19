f54bc9a8e97748f24f6fc46cd0d3cb3279ef1c1e
Otto Perdeck <perdo@pega.com>
2018-03-19 15:45:30 +0100

    Added AUC and Pega date-time conversion functions

--- r/R/cdh_utils.R
+++ r/R/cdh_utils.R
@@ -44,6 +44,49 @@ readDSExport <- function(instancename, srcFolder=".", tmpFolder=srcFolder)
   return(ds [, names(ds)[!sapply(ds, is_list)], with=F])
 }
 
+#' Calculates AUC from an array of truth values and predictions.
+#'
+#' Calculates the area under the ROC curve from an array of truth values and predictions, making sure to
+#' always return a value between 0.5 and 1.0 and will return 0.5 in case of any issues.
+#'
+#' @param groundtruth The 'true' values, with just 2 different values, passed on to the pROC::roc function.
+#' @param probs The predictions, as a numeric vector as the same length as \code{groundtruth}
+#'
+#' @return The AUC as a value between 0.5 and 1, return 0.5 if there are any issues with the data.
+#' @export
+#'
+#' @examples
+#' safe_auc( c("yes", "yes", "no"), c(0.6, 0.2, 0.2))
+safe_auc <- function(groundtruth, probs)
+{
+  if (length(unique(groundtruth)) < 2) { return (0.5) }
+  auc <- as.numeric(pROC::auc(groundtruth, probs))
+  return (ifelse(is.na(auc),0.5,(0.5+abs(0.5-auc))))
+}
+
+#' Calculates AUC from counts of positives and negatives directly.
+#'
+#' This is an efficient calculation of the AUC directly from an array of positives and negatives. It makes
+#' sure to always return a value between 0.5 and 1.0 and will return 0.5 in case of any issues.
+#'
+#' @param pos Vector with counts of the positive responses
+#' @param neg Vector with counts of the negative responses
+#'
+#' @return The AUC as a value between 0.5 and 1, return 0.5 if there are any issues with the data.
+#' @export
+#'
+#' @examples
+#' dsm_auc( c(3,1,0), c(2,0,1))
+dsm_auc <- function(pos, neg)
+{
+  o <- order(pos/(pos+neg), decreasing = T)
+  FPR <- rev(cumsum(neg[o]) / sum(neg))
+  TPR <- rev(cumsum(pos[o]) / sum(pos))
+  Area <- (FPR-c(FPR[-1],0))*(TPR+c(TPR[-1],0))/2
+  auc <- sum(Area)
+  return (ifelse(is.na(auc),0.5,(0.5+abs(0.5-auc))))
+}
+
 #' Convert AUC performance metric to GINI.
 #'
 #' @param auc The AUC (number between 0.5 and 1)
@@ -57,4 +100,36 @@ auc2GINI <- function(auc)
 {
   return (2*auc-1)
 }
+
+#' Convert from a Pega date-time string.
+#'
+#' @param x A vector of Pega date-time strings
+#'
+#' @return An object of class \linkS4class{POSIXlt}
+#' @export
+#'
+#' @examples
+#' fromPRPCDateTime("20180316T134127.847 GMT")
+fromPRPCDateTime <- function(x)
+{
+  op <- options(digits.secs=3) # for the milliseconds; the %OS3 is not universally supported so doing via options
+  t <- strptime(x, format="%Y%m%dT%H%M%OS", tz="GMT")
+  options(op)
+  return(t)
+}
+
+#' Convert to a Pega date-time string.
+#'
+#' @param x A vector of valid date-time objects (valid for \code{\link[base]{strftime}})
+#'
+#' @return A vector of string representations in the format used by Pega.
+#' @export
+#'
+#' @examples
+#' toPRPCDateTime( lubridate::now() )
+toPRPCDateTime <- function(x)
+{
+  return(strftime(x, format="%Y%m%dT%H%M%OS3", tz="GMT", usetz=T))
+}
+
 
--- r/tests/testthat/test_cdh_utils.R
+++ r/tests/testthat/test_cdh_utils.R
@@ -1,5 +1,6 @@
 library(testthat)
 library(cdhtools)
+library(lubridate)
 
 context("check something")
 
@@ -15,3 +16,37 @@ test_that("dataset export can be read", {
 # devtools::use_data(admdatamart_models)
 # save(admdatamart_models, file="data/admdatamart_models.rda", compress='xz')
 # + describe in R/data.R
+
+test_that("AUC from binning", {
+  expect_equal(dsm_auc( c(3,1,0), c(2,0,1)), 0.75)
+
+  # This actually is an example from the COC Mesh article
+  positives <- c(50,70,75,80,85,90,110,130,150,160)
+  negatives <- c(1440,1350,1170,990,810,765,720,675,630,450)
+
+  expect_equal(dsm_auc(positives, negatives), 0.6871)
+  expect_equal(dsm_auc(positives, rep(0, length(positives))), 0.5)
+})
+
+test_that("AUC from full arrays", {
+  expect_equal(safe_auc( c("yes", "yes", "no"), c(0.6, 0.2, 0.2)), 0.75)
+
+  # from https://www.r-bloggers.com/calculating-auc-the-area-under-a-roc-curve/
+  category <- c(1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0)
+  prediction <- rev(seq_along(category))
+  prediction[9:10] <- mean(prediction[9:10])
+  expect_equal(safe_auc(category, prediction), 0.825)
+
+  # This actually is an example from the COC Mesh article
+  # these will be turned into a lengthy format of 10.000 responses
+  positives <- c(50,70,75,80,85,90,110,130,150,160)
+  negatives <- c(1440,1350,1170,990,810,765,720,675,630,450)
+
+  truth <- unlist(sapply(seq(length(positives)), function(i){ return(c(rep(1, positives[i]), rep(0, negatives[i])))}))
+  probs <- unlist(sapply(seq(length(positives)), function(i){ return(c(rep(positives[i]/(positives[i]+negatives[i]), positives[i]+negatives[i])))}))
+
+  expect_equal(safe_auc(truth, probs), 0.6871)
+  expect_equal(safe_auc(truth, rep(0, length(probs))), 0.5)
+})
+
+
