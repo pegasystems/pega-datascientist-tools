3966284c50929369ed1e49bcd5b1a28c98c2541f
Otto Perdeck <perdo@pega.com>
2018-03-20 21:23:51 +0100

    Added date/time funcs and simple vignette about DS export

--- r/R/cdh_utils.R
+++ r/R/cdh_utils.R
@@ -18,12 +18,13 @@ NULL
 
 #' Read a Pega dataset export file.
 #'
-#' \code{readDSExport} reads a dataset export file as exported and downloaded from Pega. The file
+#' \code{readDSExport} reads a dataset export file as exported and downloaded from Pega. The export file
 #' is formatted as a zipped mulit-line JSON file (\url{http://jsonlines.org/}). \code{readDSExport} will find the
-#' most recent file (Pega appends a datetime stamp), unzip it into a temp folder
-#' and read the data into a \code{data.table}.
+#' most recent file (Pega appends a datetime stamp), unzip it into a temp folder and read the data into a
+#' \code{data.table}.
 #'
-#' @param instancename Name of the file w/o the timestamp, in Pega format <Applies To>_<Instance Name>
+#' @param instancename Name of the file w/o the timestamp, in Pega format <Applies To>_<Instance Name>, or the
+#' complete filename including timestamp and zip extension as exported from Pega.
 #' @param srcFolder Optional folder to look for the file (defaults to the current folder)
 #' @param tmpFolder Optional folder to store the unzipped data (defaults to the source folder)
 #'
@@ -33,15 +34,28 @@ NULL
 #' @examples
 #' \dontrun{readDSExport("Data-Decision-ADM-ModelSnapshot_AllModelSnapshots")}
 #' \dontrun{readDSExport("Data-Decision-ADM-ModelSnapshot_AllModelSnapshots", "~/Downloads")}
+#' \dontrun{readDSExport("Data-Decision-ADM-ModelSnapshot_AllModelSnapshots_20180316T135038_GMT.zip", "~/Downloads")}
 readDSExport <- function(instancename, srcFolder=".", tmpFolder=srcFolder)
 {
-  mostRecentZip <- rev(sort(list.files(path=srcFolder, pattern=paste("^", instancename, "_.*\\.zip$", sep=""))))[1]
+  if(file.exists(paste(srcFolder,instancename,sep="/"))) {
+    mostRecentZip <- instancename
+  } else {
+    mostRecentZip <- rev(sort(list.files(path=srcFolder, pattern=paste("^", instancename, "_.*\\.zip$", sep=""))))[1]
+    if(!file.exists(paste(srcFolder,mostRecentZip,sep="/"))) stop("File not found")
+  }
   jsonFile <- paste(tmpFolder,"data.json", sep="/")
   if(file.exists(jsonFile)) file.remove(jsonFile)
   utils::unzip(paste(srcFolder,mostRecentZip,sep="/"), exdir=tmpFolder)
   multiLineJSON <- readLines(jsonFile)
+  file.remove(jsonFile)
   ds <- data.table(jsonlite::fromJSON(paste("[",paste(multiLineJSON,sep="",collapse = ","),"]")))
   return(ds [, names(ds)[!sapply(ds, is_list)], with=F])
+}
+
+# Internal helper to keep auc a safe number between 0.5 and 1.0 always
+safe_range_auc <- function(auc)
+{
+  return (ifelse(is.na(auc),0.5,(0.5+abs(0.5-auc))))
 }
 
 #' Calculates AUC from an array of truth values and predictions.
@@ -56,12 +70,12 @@ readDSExport <- function(instancename, srcFolder=".", tmpFolder=srcFolder)
 #' @export
 #'
 #' @examples
-#' safe_auc( c("yes", "yes", "no"), c(0.6, 0.2, 0.2))
-safe_auc <- function(groundtruth, probs)
+#' auc_from_probs( c("yes", "yes", "no"), c(0.6, 0.2, 0.2))
+auc_from_probs <- function(groundtruth, probs)
 {
   if (length(unique(groundtruth)) < 2) { return (0.5) }
   auc <- as.numeric(pROC::auc(groundtruth, probs))
-  return (ifelse(is.na(auc),0.5,(0.5+abs(0.5-auc))))
+  return (safe_range_auc(auc))
 }
 
 #' Calculates AUC from counts of positives and negatives directly.
@@ -76,15 +90,14 @@ safe_auc <- function(groundtruth, probs)
 #' @export
 #'
 #' @examples
-#' dsm_auc( c(3,1,0), c(2,0,1))
-dsm_auc <- function(pos, neg)
+#' auc_from_bincounts( c(3,1,0), c(2,0,1))
+auc_from_bincounts <- function(pos, neg)
 {
   o <- order(pos/(pos+neg), decreasing = T)
   FPR <- rev(cumsum(neg[o]) / sum(neg))
   TPR <- rev(cumsum(pos[o]) / sum(pos))
   Area <- (FPR-c(FPR[-1],0))*(TPR+c(TPR[-1],0))/2
-  auc <- sum(Area)
-  return (ifelse(is.na(auc),0.5,(0.5+abs(0.5-auc))))
+  return (safe_range_auc(sum(Area)))
 }
 
 #' Convert AUC performance metric to GINI.
@@ -98,30 +111,34 @@ dsm_auc <- function(pos, neg)
 #' auc2GINI(0.8232)
 auc2GINI <- function(auc)
 {
-  return (2*auc-1)
+  return (2*safe_range_auc(auc)-1)
 }
 
 #' Convert from a Pega date-time string.
 #'
+#' The timezone string is taken into account but it assumed that they are the same for all the strings. If
+#' this is not a valid assumption, \code{apply} this function to your data instead of passing in a vector.
+#'
 #' @param x A vector of Pega date-time strings
 #'
-#' @return An object of class \linkS4class{POSIXlt}
+#' @return An object of class \code{POSIXct} (this the compact POSIXt class, the lt is the list variation)
 #' @export
 #'
 #' @examples
 #' fromPRPCDateTime("20180316T134127.847 GMT")
+#' fromPRPCDateTime("20180316T184127.846")
 fromPRPCDateTime <- function(x)
 {
   op <- options(digits.secs=3) # for the milliseconds; the %OS3 is not universally supported so doing via options
-  t <- strptime(x, format="%Y%m%dT%H%M%OS", tz="GMT")
+  timezonesplits <- data.table::tstrsplit(x, " ")
+  if(length(timezonesplits)>1) {
+    t <- strptime(x, format="%Y%m%dT%H%M%OS", tz=timezonesplits[[2]][1])
+  } else {
+    t <- strptime(x, format="%Y%m%dT%H%M%OS")
+  }
   options(op)
-  return(t)
+  return(as.POSIXct(t))
 }
-
-# TODO comvert to ct as.POSIXct
-
-# TODO dont need lubridate
-# as.POSIXlt(Sys.time(), "GMT") # the current time in UTC
 
 #' Convert to a Pega date-time string.
 #'
@@ -131,7 +148,7 @@ fromPRPCDateTime <- function(x)
 #' @export
 #'
 #' @examples
-#' toPRPCDateTime( lubridate::now() )
+#' toPRPCDateTime(Sys.time())
 toPRPCDateTime <- function(x)
 {
   return(strftime(x, format="%Y%m%dT%H%M%OS3", tz="GMT", usetz=T))
--- r/tests/testthat/test_cdh_utils.R
+++ r/tests/testthat/test_cdh_utils.R
@@ -10,11 +10,18 @@ test_that("dataset export can be read", {
 })
 
 # to add data:
+
 # admdatamart_models <- readDSExport("Data-Decision-ADM-ModelSnapshot_AllModelSnapshots", "~/Downloads")
 # names(admdatamart_models) <- tolower(names(admdatamart_models))
 # for(f in c("pyperformance")) admdatamart_models[[f]] <- as.numeric(admdatamart_models[[f]])
 # devtools::use_data(admdatamart_models)
 # save(admdatamart_models, file="data/admdatamart_models.rda", compress='xz')
+#
+# admdatamart_binning <- readDSExport("Data-Decision-ADM-ModelSnapshot_AllModelSnapshots", "~/Downloads")
+# names(admdatamart_binning) <- tolower(names(admdatamart_binning))
+# for(f in c("pyperformance")) admdatamart_binning[[f]] <- as.numeric(admdatamart_binning[[f]])
+# devtools::use_data(admdatamart_binning)
+# save(admdatamart_binning, file="data/admdatamart_binning.rda", compress='xz')
 # + describe in R/data.R
 
 # ihsampledata taken from IH in DMSample after initialization
@@ -22,26 +29,27 @@ test_that("dataset export can be read", {
 #ihsampledata <- ihsampledata[ pyApplication=="DMSample"&pySubjectID %in% paste("CE", seq(1:10), sep="-")]
 #devtools::use_data(ihsampledata)
 #save(ihsampledata, file="data/ihsampledata.rda", compress='xz')
+# + describe in R/data.R
 
 test_that("AUC from binning", {
-  expect_equal(dsm_auc( c(3,1,0), c(2,0,1)), 0.75)
+  expect_equal(auc_from_bincounts( c(3,1,0), c(2,0,1)), 0.75)
 
   # This actually is an example from the COC Mesh article
   positives <- c(50,70,75,80,85,90,110,130,150,160)
   negatives <- c(1440,1350,1170,990,810,765,720,675,630,450)
 
-  expect_equal(dsm_auc(positives, negatives), 0.6871)
-  expect_equal(dsm_auc(positives, rep(0, length(positives))), 0.5)
+  expect_equal(auc_from_bincounts(positives, negatives), 0.6871)
+  expect_equal(auc_from_bincounts(positives, rep(0, length(positives))), 0.5)
 })
 
 test_that("AUC from full arrays", {
-  expect_equal(safe_auc( c("yes", "yes", "no"), c(0.6, 0.2, 0.2)), 0.75)
+  expect_equal(auc_from_probs( c("yes", "yes", "no"), c(0.6, 0.2, 0.2)), 0.75)
 
   # from https://www.r-bloggers.com/calculating-auc-the-area-under-a-roc-curve/
   category <- c(1, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0)
   prediction <- rev(seq_along(category))
   prediction[9:10] <- mean(prediction[9:10])
-  expect_equal(safe_auc(category, prediction), 0.825)
+  expect_equal(auc_from_probs(category, prediction), 0.825)
 
   # This actually is an example from the COC Mesh article
   # these will be turned into a lengthy format of 10.000 responses
@@ -51,8 +59,20 @@ test_that("AUC from full arrays", {
   truth <- unlist(sapply(seq(length(positives)), function(i){ return(c(rep(1, positives[i]), rep(0, negatives[i])))}))
   probs <- unlist(sapply(seq(length(positives)), function(i){ return(c(rep(positives[i]/(positives[i]+negatives[i]), positives[i]+negatives[i])))}))
 
-  expect_equal(safe_auc(truth, probs), 0.6871)
-  expect_equal(safe_auc(truth, rep(0, length(probs))), 0.5)
+  expect_equal(auc_from_probs(truth, probs), 0.6871)
+  expect_equal(auc_from_probs(truth, rep(0, length(probs))), 0.5)
 })
 
+test_that("GINI conversion", {
+  expect_equal(auc2GINI(0.8232), 0.6464)
+  expect_equal(auc2GINI(0.5), 0.0)
+  expect_equal(auc2GINI(1.0), 1.0)
+  expect_equal(auc2GINI(0.6), 0.2)
+  expect_equal(auc2GINI(0.4), 0.2)
+  expect_equal(auc2GINI(NA), 0.0)
+})
 
+test_that("Date conversion", {
+  expect_equal(toPRPCDateTime(fromPRPCDateTime("20180316T134127.847")), "20180316T124127.846 GMT")
+  expect_equal(toPRPCDateTime(fromPRPCDateTime("20180316T000000.000 EST")), "20180316T050000.000 GMT")
+})
