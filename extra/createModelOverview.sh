74bc5ca5880f6a2028302d735fc52815eb8e5112
Perdeck, Otto <Otto.Perdeck@pega.com>
2019-07-23 18:05:38 +0200

    Generic notebook to create model overview report

--- extra/createModelOverview.sh
+++ extra/createModelOverview.sh
@@ -5,10 +5,8 @@
 # Source needs to be a CSV file from the model table
 
 notebook="./adaptivemodeloverview.Rmd"
-#source="../extra/pr_data_dm_admmart_pred.csv"
-source="~/Box Sync/Customers/RABO/datamart july 2019/PR_DATA_DM_ADMMART_MDL_FACT.csv.zip"
-modelconfig="pipodeclown"
-output="`pwd`/$modelconfig.html"
+source="../extra/pr_data_dm_admmart_mdl_fact.csv"
+output="`pwd`/Adaptive\ Models\ Overview.html"
 
-R -e "rmarkdown::render('$notebook',params = list(modelfile='$source', modelconfig='$modelconfig'), output_file='$output')"
+R -e "rmarkdown::render('$notebook',params = list(modelfile='$source'), output_file='$output')"
 
