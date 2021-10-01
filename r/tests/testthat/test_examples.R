6dd55f331e90594477e51cd824fe628a1eb3b357
perdo <otto.perdeck@pega.com>
2021-10-01 16:43:02 +0200

    Canonical paths for testing toplevel examples does not really work

--- r/tests/testthat/test_examples.R
+++ r/tests/testthat/test_examples.R
@@ -2,18 +2,26 @@ library(testthat)
 
 context("Toplevel Examples")
 
-test_that("example folder exists", {
-  exampleFolder <- normalizePath(file.path("../../..", "examples"))
+# > test_check("cdhtools")
+# [1] "Example folder check"
+# [1] "C:/Users/runneradmin/AppData/Local/Temp/RtmpYJIkSb/file442fbea53/cdhtools.Rcheck/tests/testthat"
+# [1] "C:\\Users\\runneradmin\\AppData\\Local\\Temp\\RtmpYJIkSb\\file442fbea53\\examples"
 
-  print("Example folder check")
-  print(getwd())
-  print(exampleFolder)
+# Looks like we only have access to the test folder and down from
+# GitHub actions - they're running in total isolation
 
-  # p <- normalizePath(exampleFolder, mustWork = T)
-  # print(p)
-
-  expect_true(dir.exists(exampleFolder))
-})
+# test_that("example folder exists", {
+#   exampleFolder <- normalizePath(file.path("../../..", "examples"))
+#
+#   print("Example folder check")
+#   print(getwd())
+#   print(exampleFolder)
+#
+#   # p <- normalizePath(exampleFolder, mustWork = T)
+#   # print(p)
+#
+#   expect_true(dir.exists(exampleFolder))
+# })
 
 # test_that("expected R notebooks are there", {
 #   notebooks <- list.files(exampleFolder, pattern=".Rmd", recursive = T, full.names = T)
