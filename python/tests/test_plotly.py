36908d6647182ec519b9b126efe8c624ce55ca0a
Stijn Kas <stijn.kas@pega.com>
2022-03-21 15:00:59 +0100

    PredictorBinning raises error if no models

--- python/tests/test_plotly.py
+++ python/tests/test_plotly.py
@@ -52,6 +52,13 @@ def test_plotPredictorBinning(test):
         predictors=["AGE"],
     )
 
+def test_plotPredictorBinning_none_found(test):
+    with pytest.raises(ValueError):
+        test.plotPredictorBinning(
+            modelid="unknown",
+            predictors=["AGE"],
+    )
+
 
 def test_PredictorPerformance(test):
     test.plotPredictorPerformance(top_n=5)
