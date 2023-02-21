e171a69e7432f87966949902b8b85f6bd49af127
Stijn Kas <stijn.kas@pega.com>
2023-02-21 16:36:50 +0100

    Added hds_utils to __init__

--- python/pdstools/__init__.py
+++ python/pdstools/__init__.py
@@ -1,5 +1,4 @@
 from .adm.ADMDatamart import ADMDatamart
 from .adm.ADMTrees import ADMTrees, MultiTrees
 from .valuefinder.ValueFinder import ValueFinder
-from .utils import cdh_utils
-from .utils import datasets
+from .utils import cdh_utils, datasets, hds_utils
