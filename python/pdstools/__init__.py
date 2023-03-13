097c3798550af8c4b13088743d482d972abc727e
Stijn Kas <stijn.kas@pega.com>
2023-03-13 16:55:41 +0100

    Add global stringcache to `__init__.py`

--- python/pdstools/__init__.py
+++ python/pdstools/__init__.py
@@ -1,6 +1,9 @@
 """Python pdstools."""
 
 __version__ = "3.0.0"
+from polars.polars import toggle_string_cache
+
+toggle_string_cache(True)
 
 from .adm.ADMDatamart import ADMDatamart
 from .adm.ADMTrees import ADMTrees, MultiTrees
