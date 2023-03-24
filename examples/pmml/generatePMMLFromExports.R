a388c99c04a89e4d6e16001d6f08c0f7b6bbd6b9
Stijn Kas <78410144+StijnKas@users.noreply.github.com>
2023-03-24 18:10:25 +0100

    Delete examples directory

--- examples/pmml/generatePMMLFromExports.R
+++
@@ -1,9 +0,0 @@
-# Export ADM Datamart models to PMML from a dataset export
-
-library(XML)
-library(pdstools)
-
-models <- readDSExport("Data-Decision-ADM-ModelSnapshot", srcFolder="~/Downloads", tmpFolder="tmp")
-predictors <- readDSExport("Data-Decision-ADM-PredictorBinningSnapshot", srcFolder="~/Downloads", tmpFolder="tmp")
-
-adm2pmml(models, predictors, verbose=T, destDir="~/Downloads", ruleNameFilter = "^(?!VerySimple).*$")
