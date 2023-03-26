45ee431b85ac4b11aab887fbd63cc5a5ae9f3def
Stijn Kas <stijn.kas@pega.com>
2023-03-26 20:11:38 +0200

    Test healthcheck runs without issues

---
+++ python/tests/test_healthcheck.py
@@ -0,0 +1,15 @@
+from pdstools import datasets
+import pytest
+
+@pytest.fixture
+def sample():
+    return datasets.CDHSample()
+
+def testHealthCheckRunsWithoutErrors(sample):
+    sample.generateHealthCheck(verbose=True)
+
+def testHealthCheckRunsWithoutTables(sample):
+    sample.generateHealthCheck(include_tables=False)
+
+def testAdditionalTables(sample):
+    sample.exportTables()
