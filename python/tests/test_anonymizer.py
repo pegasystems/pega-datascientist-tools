4717e4d6dc1e315758fb2af1f579fc489b9649a5
Stijn Kas <stijn.kas@pega.com>
2023-04-21 17:00:05 +0200

    Fix: Replace empty string in numeric columns with Null

--- python/tests/test_anonymizer.py
+++ python/tests/test_anonymizer.py
@@ -3,6 +3,7 @@ import sys
 import polars as pl
 
 import pathlib
+
 basePath = pathlib.Path(__file__).parent.parent.parent
 sys.path.append(f"{str(basePath)}/python")
 from pdstools.utils.hds_utils import Config, DataAnonymization
@@ -14,14 +15,14 @@ def sampleInput():
     from io import StringIO
 
     input = StringIO(
-        """{"Context_Name":"FirstMortgage30yr","Customer_MaritalStatus":"Married","Customer_CLV":1460,"Customer_City":"Port Raoul","IH_Web_Inbound_Accepted_pxLastGroupID":"Account","Decision_Outcome":"Rejected"}
-        {"Context_Name":"FirstMortgage30yr","Customer_MaritalStatus":"Unknown","Customer_CLV":669,"Customer_City":"Laurianneshire","IH_Web_Inbound_Accepted_pxLastGroupID":"AutoLoans","Decision_Outcome":"Accepted"}
-        {"Context_Name":"MoneyMarketSavingsAccount","Customer_MaritalStatus":"No Resp+","Customer_CLV":1174,"Customer_City":"Jacobshaven","IH_Web_Inbound_Accepted_pxLastGroupID":"Account","Decision_Outcome":"Rejected"}
-        {"Context_Name":"BasicChecking","Customer_MaritalStatus":"Unknown","Customer_CLV":1476,"Customer_City":"Lindton","IH_Web_Inbound_Accepted_pxLastGroupID":"Account","Decision_Outcome":"Rejected"}
-        {"Context_Name":"BasicChecking","Customer_MaritalStatus":"Married","Customer_CLV":1211,"Customer_City":"South Jimmieshire","IH_Web_Inbound_Accepted_pxLastGroupID":"DepositAccounts","Decision_Outcome":"Accepted"}
-        {"Context_Name":"UPlusFinPersonal","Customer_MaritalStatus":"No Resp+","Customer_CLV":533,"Customer_City":"Bergeville","IH_Web_Inbound_Accepted_pxLastGroupID":null,"Decision_Outcome":"Rejected"}
-        {"Context_Name":"BasicChecking","Customer_MaritalStatus":"No Resp+","Customer_CLV":555,"Customer_City":"Willyville","IH_Web_Inbound_Accepted_pxLastGroupID":"DepositAccounts","Decision_Outcome":"Rejected"}
-        """
+        """{"Context_Name":"FirstMortgage30yr","Customer_MaritalStatus":"Married","Customer_CLV":"1460","Customer_City":"Port Raoul","IH_Web_Inbound_Accepted_pxLastGroupID":"Account","Decision_Outcome":"Rejected"}
+            {"Context_Name":"FirstMortgage30yr","Customer_MaritalStatus":"Unknown","Customer_CLV":"669","Customer_City":"Laurianneshire","IH_Web_Inbound_Accepted_pxLastGroupID":"AutoLoans","Decision_Outcome":"Accepted"}
+            {"Context_Name":"MoneyMarketSavingsAccount","Customer_MaritalStatus":"No Resp+","Customer_CLV":"1174","Customer_City":"Jacobshaven","IH_Web_Inbound_Accepted_pxLastGroupID":"Account","Decision_Outcome":"Rejected"}
+            {"Context_Name":"BasicChecking","Customer_MaritalStatus":"Unknown","Customer_CLV":"1476","Customer_City":"Lindton","IH_Web_Inbound_Accepted_pxLastGroupID":"Account","Decision_Outcome":"Rejected"}
+            {"Context_Name":"BasicChecking","Customer_MaritalStatus":"Married","Customer_CLV":"","Customer_City":"South Jimmieshire","IH_Web_Inbound_Accepted_pxLastGroupID":"DepositAccounts","Decision_Outcome":"Accepted"}
+            {"Context_Name":"UPlusFinPersonal","Customer_MaritalStatus":"No Resp+","Customer_CLV":"533","Customer_City":"Bergeville","IH_Web_Inbound_Accepted_pxLastGroupID":null,"Decision_Outcome":"Rejected"}
+            {"Context_Name":"BasicChecking","Customer_MaritalStatus":"No Resp+","Customer_CLV":"555","Customer_City":"Willyville","IH_Web_Inbound_Accepted_pxLastGroupID":"DepositAccounts","Decision_Outcome":"Rejected"}
+            """
     )
     return pl.read_ndjson(input)
 
@@ -43,7 +44,7 @@ def testDefault(sampleInput):
         0.14422057264050903,
         0.679745493107105,
         1.0,
-        0.71898197242842,
+        None,
         0.0,
         0.02332979851537646,
     ]
