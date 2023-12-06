434e915cfcad63fff5af65440b1120f90288cd61
Uyanik, Yusuf <gokturk113@gmail.com>
2023-12-06 11:09:03 +0100

    remove context_keys selection in app

--- python/pdstools/app/pages/1-Data Import.py
+++ python/pdstools/app/pages/1-Data Import.py
@@ -11,24 +11,16 @@ in the expanding section below."""
 
 opts = {}
 with st.expander("Configure advanced options", expanded=True):
-    col1, col2 = st.columns([4, 7])
-    with col1:
-        opts["extract_keys"] = st.checkbox(
-            "Extract Treatments",
-            True,
-            help="""By default, ADM has a few "Context Keys" it uses to 
-            distinguish between models, such as Issue, Group, Channel, or Name. 
-            However, if you've setup custom context keys that are not part of a regular 
-            CDH setup, they are embedded in the "pyName" column. Setting this checkbox
-            tells us to try to extract these keys into separate columns.""",
-        )
-    with col2:
-        default_keys = ["Channel", "Direction", "Issue", "Group"]
-        if opts["extract_keys"]:
-            default_keys = default_keys + ["Treatment"]
-        opts["context_keys"] = st.multiselect(
-            "Select the keys used as context keys", default_keys, default_keys
-        )
+    opts["extract_keys"] = st.checkbox(
+        "Extract Treatments",
+        True,
+        help="""By default, ADM has a few "Context Keys" it uses to 
+        distinguish between models, such as Issue, Group, Channel, or Name. 
+        However, if you've setup custom context keys that are not part of a regular 
+        CDH setup, they are embedded in the "pyName" column. Setting this checkbox
+        tells us to try to extract these keys into separate columns.""",
+    )
+
 
 streamlit_utils.import_datamart(**opts)
 for key in st.session_state.keys():
