43062cb4f26479500f6e9236dd9a07934b378b98
perdo <otto.perdeck@pega.com>
2024-01-13 14:17:19 +0100

    Improved coverage

--- python/tests/test_healthcheck.py
+++ python/tests/test_healthcheck.py
@@ -1,7 +1,7 @@
 import sys
 import os
-
 import pathlib
+from pandas import read_excel, ExcelFile
 
 basePath = pathlib.Path(__file__).parent.parent.parent
 sys.path.append(f"{str(basePath)}/python")
@@ -14,15 +14,6 @@ def sample():
     return datasets.CDHSample()
 
 
-def testHealthCheckRunsWithoutErrors(sample):
-    sample.generateReport(verbose=True)
-
-
-def testAdditionalTables(sample):
-    sample.exportTables(predictorBinning=True)
-    os.remove("Tables.xlsx")
-
-
 @pytest.fixture
 def sample_without_predictorbinning():
     return ADMDatamart(
@@ -32,20 +23,79 @@ def sample_without_predictorbinning():
     )
 
 
-def testHealthCheckModelRunsWithoutErrors(sample_without_predictorbinning):
-    sample_without_predictorbinning.generateReport(verbose=True, modelData_only=True)
+def test_GenerateHealthCheck(sample):
+    hc = sample.generateReport(verbose=True)
+    assert hc == "./HealthCheck.html"
+    assert pathlib.Path(hc).exists()
+    os.remove("HealthCheck.html")
+    assert not pathlib.Path(hc).exists()
+
+def test_ExportTables(sample):
+    excel = sample.exportTables(predictorBinning=True)
+    assert excel == "Tables.xlsx"
+    assert pathlib.Path(excel).exists()
+    spreadsheet = ExcelFile(excel)
+    assert list(spreadsheet.sheet_names) == [
+        "modeldata_last_snapshot",
+        "predictor_last_snapshot",
+        "predictorbinning",
+    ]
+    # TODO we could go further and check the size of the sheets
+    # spreadsheet = read_excel(excel, sheet_name=None)
+    os.remove("Tables.xlsx")
+    assert not pathlib.Path(excel).exists()
+
+def test_ExportTables_NoBinning(sample):
+    excel = sample.exportTables(predictorBinning=False)
+    assert excel == "Tables.xlsx"
+    assert pathlib.Path(excel).exists()
+    spreadsheet = ExcelFile(excel)
+    assert list(spreadsheet.sheet_names) == [
+        "modeldata_last_snapshot",
+        "predictor_last_snapshot",
+    ]
+    # TODO we could go further and check the size of the sheets
+    # spreadsheet = read_excel(excel, sheet_name=None)
+    os.remove("Tables.xlsx")
+    assert not pathlib.Path(excel).exists()
+
+def test_GenerateHealthCheck_ModelDataOnly(sample_without_predictorbinning):
+    hc = sample_without_predictorbinning.generateReport(
+        name="MyOrg", verbose=True, modelData_only=True
+    )
+    assert hc == "./HealthCheck_MyOrg.html"
+    assert pathlib.Path(hc).exists()
+    os.remove("HealthCheck_MyOrg.html")
+    assert not pathlib.Path(hc).exists()
 
 
-def testAdditionalTablesModel(sample_without_predictorbinning):
-    sample_without_predictorbinning.exportTables(
+def test_ExportTables_ModelDataOnly(sample_without_predictorbinning):
+    excel = sample_without_predictorbinning.exportTables(
         file="ModelTables.xlsx", predictorBinning=True
     )
+    assert excel == "ModelTables.xlsx"
+    assert pathlib.Path(excel).exists()
+    spreadsheet = ExcelFile(excel)
+    assert list(spreadsheet.sheet_names) == [
+        "modeldata_last_snapshot",
+    ]
+    # TODO we could go further and check the size of the sheets
+    # spreadsheet = read_excel(excel, sheet_name=None)
     os.remove("ModelTables.xlsx")
 
 
-def modelReportRuns(sample):
-    sample.generateReport(modelid="bd70a915-697a-5d43-ab2c-53b0557c85a0")
+def test_GenerateModelReport(sample):
+    report = sample.generateReport(
+        name="MyOrg", modelid="bd70a915-697a-5d43-ab2c-53b0557c85a0"
+    )
+    assert report == "./ModelReport_MyOrg_bd70a915-697a-5d43-ab2c-53b0557c85a0.html"
+    assert pathlib.Path(report).exists()
+    os.remove(report)
+    assert not pathlib.Path(report).exists()
 
-
-def remove_healthCheck():
-    os.remove("ADM_HealthCheck.html")
+def test_GenerateModelReport_Failing(sample_without_predictorbinning):
+    with pytest.raises(Exception) as e_info:
+        sample_without_predictorbinning.generateReport(
+            name="MyOrg", modelid="bd70a915-697a-5d43-ab2c-53b0557c85a0"
+        )
+    assert "Error when generating healthcheck" in str(e_info)
--- python/tests/test_show_versions.py
+++ python/tests/test_show_versions.py
@@ -2,17 +2,19 @@
 Testing the functionality of utils/show_versions functions
 """
 
-import datetime
 import pathlib
 import sys
 
-import numpy as np
-import polars as pl
-import pytest
-from pytz import timezone
-
 basePath = pathlib.Path(__file__).parent.parent.parent
 sys.path.append(f"{str(basePath)}/python")
-from pdstools import cdh_utils, datasets
+import pdstools
 
 
+def test_show_versions(capsys):
+    pdstools.show_versions()
+    captured = capsys.readouterr()
+    assert "pdstools" in captured.out
+    assert "---Version info---" in captured.out
+    assert "---Dependencies---" in captured.out
+    assert "---Streamlit app dependencies---" in captured.out
+    
