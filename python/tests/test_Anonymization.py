afce1279304d0d9bac0b337117483454876c5a10
Stijn Kas <78410144+StijnKas@users.noreply.github.com>
2024-08-29 17:48:13 +0200

    Updated and heavily simplified anonymization script (#235)
    * Updated and heavily simplified anonymzation script
    
    * Fixed inits
    
    * one more leftover import
    
    * remove old tests
    
    * Minor changes to the anonymisation script (#236)
    
    * Preprocess now returns a list of paths to the temporary parquet files it created
    
    * Added type hint
    
    ---------
    
    Co-authored-by: middd2 <Daniel.Middelhede@pega.com>
    
    * Update type hints to support 3.8
    
    * Use self variable
    
    * Add basic tests
    
    * polars-hash as dependency
    
    * improve coverage
    
    * one more cov skip
    
    * retriggering CI before merge
    
    ---------
    
    Co-authored-by: Daniel M <dmiddelhede@gmail.com>
    Co-authored-by: middd2 <Daniel.Middelhede@pega.com>

---
+++ python/tests/test_Anonymization.py
@@ -0,0 +1,68 @@
+import os
+import pathlib
+import sys
+
+import polars as pl
+import pytest
+
+basePath = pathlib.Path(__file__).parent.parent.parent
+sys.path.append(f"{str(basePath)}/python")
+from pdstools import Anonymization
+
+
+@pytest.fixture
+def anonymizer():
+    return Anonymization(
+        path_to_files=f"{basePath}/data/SampleHDS.json",
+        temporary_path="tmp",
+        output_file="anonymised.parquet",
+        skip_columns_with_prefix=["Context_", "Decision_"],
+        batch_size=1000,
+        file_limit=10,
+    )
+
+
+def test_anonymize(anonymizer):
+    anonymizer.anonymize(verbose=True)
+    assert os.path.exists(anonymizer.output_file)
+
+
+def test_min_max():
+    range = [{"min": 0.0, "max": 100.0}]
+    expr = Anonymization.min_max("age", range)
+    assert isinstance(expr, pl.Expr)
+
+
+def test_infer_types():
+    df = pl.DataFrame(
+        {"col1": [1, 2, 3], "col2": ["a", "b", "c"], "col3": [1.0, 2.0, 3.0]}
+    )
+    types = Anonymization._infer_types(df)
+    assert types == {"col1": "numeric", "col2": "symbolic", "col3": "numeric"}
+
+
+def test_chunker():
+    files = ["file1.txt", "file2.txt", "file3.txt", "file4.txt", "file5.txt"]
+    chunks = list(Anonymization.chunker(files, 2))
+    assert chunks == [
+        ["file1.txt", "file2.txt"],
+        ["file3.txt", "file4.txt"],
+        ["file5.txt"],
+    ]
+
+
+def test_chunk_to_parquet(anonymizer):
+    files = [f"{basePath}/data/SampleHDS.json"]
+    filename = anonymizer.chunk_to_parquet(files, 0)
+    assert os.path.exists(filename)
+
+
+def test_preprocess(anonymizer):
+    chunked_files = anonymizer.preprocess(verbose=False)
+    assert len(chunked_files) > 0
+
+
+def test_process(anonymizer):
+    chunked_files = anonymizer.preprocess(verbose=False)
+    anonymizer.process(chunked_files, verbose=False)
+    assert os.path.exists(anonymizer.output_file)
