ebd95a4d873674fa70cabea7e44c464c38cd5dbb
Stijn Kas <78410144+StijnKas@users.noreply.github.com>
2024-11-04 15:24:12 +0100

    Update OAuth instructions
    The instructions for creating the OAuth client were incorrect - it should be OAuth 2.0 Client Registration rather than OAuth Client.

--- python/pdstools/infinity/client.py
+++ python/pdstools/infinity/client.py
@@ -12,7 +12,7 @@ and it assumes the same permissions as the operator with which you've logged in.
 
 `.from_client_credentials` uses an OAuth credentials file,
 which you can create by first going to Dev Studio, then navigating to
-Create -> Security -> OAuth Client. If the OAuth Client does not show up in the
+Create -> Security -> OAuth 2.0 Client Registration. If the OAuth Client does not show up in the
 Security tab, this is likely due to insufficient permissions for your operator.
 """
 
