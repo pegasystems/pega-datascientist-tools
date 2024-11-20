e750e730c668ca8fb1a5f3f82a1842e2d2e9a9a0
Otto Perdeck <otto.perdeck@pega.com>
2024-11-20 17:41:09 +0100

    ChannelDirectionGroup should not show Other/Inbound

--- python/pdstools/adm/Aggregates.py
+++ python/pdstools/adm/Aggregates.py
@@ -447,6 +447,8 @@ class Aggregates:
                 ChannelDirectionGroup=pl.when(
                     pl.col("ChannelGroup").is_not_null()
                     & pl.col("DirectionGroup").is_not_null()
+                    & pl.col("ChannelGroup").is_in(["Other", "Unknown", ""]).not_()
+                    & pl.col("DirectionGroup").is_in(["Other", "Unknown", ""]).not_()
                 )
                 .then(pl.concat_str(["ChannelGroup", "DirectionGroup"], separator="/"))
                 .otherwise(pl.lit("Other")),
