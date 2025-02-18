06fe97e0e9deb0e6d1daacf8169f34eb0ff35732
kass1 <stijn.kas@pega.com>
2025-02-18 13:35:15 +0100

    fix test

--- python/tests/infinity/test_client.py
+++ python/tests/infinity/test_client.py
@@ -2,6 +2,6 @@ from pdstools.infinity import Infinity
 
 
 def test_init_without_version():
-    client = Infinity.from_client_id_and_secret("NA", "NA")
+    client = Infinity.from_client_id_and_secret("TEST_URL", "NA", "NA")
     assert not client.version
     assert not hasattr(client, "prediction_studio")
