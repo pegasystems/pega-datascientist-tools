47d1e9e5b3aefd19d817038072f57583d5788066
StijnKas <stijn.kas@me.com>
2025-09-01 15:53:47 +0200

    bring back moto

--- python/tests/test_s3.py
+++ python/tests/test_s3.py
@@ -2,10 +2,11 @@ import os
 import shutil
 
 import aioboto3
+from moto import mock_aws
 from pdstools.pega_io.S3 import S3Data
 
 
-# @mock_aws
+@mock_aws
 async def test_get_s3_files():
     # Define parameters
     bucket_name = "my-test-bucket"
