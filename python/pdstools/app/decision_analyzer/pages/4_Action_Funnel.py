ca4483bd28bad369885b3c334d4cfb2225193b94
Otto Perdeck <otto.perdeck@pega.com>
2026-02-24 15:30:42 +0100

    refactor(decision-analyzer): remove redundant local filters from Action Funnel
    The optional additional filtering in the Action Funnel sidebar duplicated
    the Global Data Filters page (page 1). Users can achieve the same result
    by applying filters there. Removes the sidebar filter UI, local_filters
    state management, and all additional_filters pass-through arguments.

--- python/pdstools/app/decision_analyzer/pages/4_Action_Funnel.py
+++ python/pdstools/app/decision_analyzer/pages/4_Action_Funnel.py
@@ -1,3 +1,4 @@
+# python/pdstools/app/decision_analyzer/pages/4_Action_Funnel.py
 import io
 
 import polars as pl
@@ -6,20 +7,14 @@ from da_streamlit_utils import (
     ensure_data,
     ensure_funnel,
     get_current_index,
-    get_data_filters,
-    show_filtered_counts,
     polars_lazyframe_hashing,
-)
-
-from pdstools.decision_analyzer.utils import (
-    get_first_level_stats,
+    stage_level_selector,
 )
 
 # TODO Have a toggle to look at it both from a Passing and Filtered perspective like in Dennis' designs
 # TODO Later, the concept of stages should be generalized - there are many more and we should not be restrictive - can we pick up from the data?
 # TODO coloring of the component filtering can be used better, now a little boring - but perhaps there will be a type, now only prop filters but there may be when rules as well
 # TODO the control for the number of filter components should move to that section, not be in the side bar
-# TODO not sure about the optional additional filtering - what is the exact use case again?
 # TODO the coloring at Action level is way to busy - maybe limit to a top-N or so, probably something we need more often in general
 # TODO be ready for the many stages - see Dennis' designs
 
@@ -29,7 +24,7 @@ from pdstools.decision_analyzer.utils import (
 This gives a view of which actions are filtered out where in the
 decision funnel, but also by what component.
 
-This helps answering questions like: Where do my “cards offers” get dropped? What gets filtered in which stage?
+This helps answering questions like: Where do my "cards offers" get dropped? What gets filtered in which stage?
 
 """
 
@@ -40,18 +35,18 @@ ensure_funnel()
 @st.cache_data(hash_funcs=polars_lazyframe_hashing)
 def decision_funnel(
     scope,
-    additional_filters,
+    level=None,
     return_df=False,
 ):
     return st.session_state.decision_data.plot.decision_funnel(
-        scope=scope, additional_filters=additional_filters, return_df=return_df
+        scope=scope, return_df=return_df
     )
 
 
 st.session_state["sidebar"] = st.sidebar
-if "local_filters" in st.session_state:
-    del st.session_state["local_filters"]
 with st.session_state["sidebar"]:
+    stage_level_selector()
+
     scope_options = st.session_state.decision_data.getPossibleScopeValues()
     stage_options = st.session_state.decision_data.getPossibleStageValues()
 
@@ -63,41 +58,6 @@ with st.session_state["sidebar"]:
         value=10,
         help="Maximum number of rows in box plots",
     )
-
-    ## Filtering UI
-
-    "### Optional additional filtering"
-
-    # TODO always save state and when returning fill in the shown options in the dropdown
-    # TODO also lets sort the columns from the df in some reasonable way - context keys first etc
-    # TODO probably need to take over this "filter_dataframe" which also shows the
-    # TODO dropdown with "Filter dataframe on"
-    st.session_state["local_filters"] = get_data_filters(
-        st.session_state.decision_data.decision_data,
-        columns=st.session_state.decision_data.getAvailableFieldsForFiltering(
-            categoricalOnly=True
-        ),
-        queries=[],
-        filter_type="local",
-    )
-
-    if st.session_state["local_filters"] != []:
-        statsBeforeExtraFilter = get_first_level_stats(
-            st.session_state.decision_data.decision_data
-        )
-        statsAfterExtraFilter = get_first_level_stats(
-            st.session_state.decision_data.decision_data,
-            st.session_state["local_filters"],
-        )
-        show_filtered_counts(statsBeforeExtraFilter, statsAfterExtraFilter)
-    else:
-        st.success("No extra data filters applied")
-
-    # st.session_state.decision_data.level = st.multiselect(
-    #     "Stage Granularity",
-    #     ["Stage Group", "Stage"],
-    #     default="Stage Group"
-    # )
 
 
 with st.container(border=True):
@@ -113,7 +73,7 @@ with st.container(border=True):
             st.session_state.scope = scope_options[0]
         remanining_funnel, filtered_funnel = decision_funnel(
             scope=st.session_state.scope,
-            additional_filters=st.session_state["local_filters"],
+            level=st.session_state.decision_data.level,
         )
         st.plotly_chart(
             remanining_funnel,
@@ -140,13 +100,11 @@ action got dropped in which stage and by what component.
 
 """
 
-data = st.session_state.decision_data.decision_data.filter(
-    pl.col("Record Type") == "FILTERED_OUT"
-)
-if st.session_state["local_filters"] != []:
-    data.filter(st.session_state["local_filters"])
 data = (
-    data.group_by(["Stage Order", "Stage Group", "Stage", "Component Name"])
+    st.session_state.decision_data.decision_data.filter(
+        pl.col("Record Type") == "FILTERED_OUT"
+    )
+    .group_by(["Stage Order", "Stage Group", "Stage", "Component Name"])
     .agg(pl.len().alias("filter count"))
     .with_columns(
         (
@@ -201,11 +159,6 @@ if has_components:
         impact_fig = st.session_state.decision_data.plot.component_action_impact(
             top_n=impact_top_n,
             scope=st.session_state.scope,
-            additional_filters=(
-                st.session_state["local_filters"]
-                if st.session_state["local_filters"] != []
-                else None
-            ),
         )
         st.plotly_chart(impact_fig, use_container_width=True)
 
@@ -246,23 +199,13 @@ if has_components:
             drilldown_fig = st.session_state.decision_data.plot.component_drilldown(
                 component_name=selected_component,
                 sort_by=sort_by,
-                additional_filters=(
-                    st.session_state["local_filters"]
-                    if st.session_state["local_filters"] != []
-                    else None
-                ),
             )
             st.plotly_chart(drilldown_fig, use_container_width=True)
 
             # Also show the raw data table
             drilldown_df = st.session_state.decision_data.getComponentDrilldown(
                 component_name=selected_component,
-                additional_filters=(
-                    st.session_state["local_filters"]
-                    if st.session_state["local_filters"] != []
-                    else None
-                ),
             )
             st.dataframe(drilldown_df)
         else:
-            st.info("No filter components found in the data.")
+            st.warning("No filter components found in the data.")
